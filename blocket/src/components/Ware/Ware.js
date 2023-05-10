cd76ab078ca3a82563f298a63d9f032bf85f9378
Tivamishae <hugo.duran0625@gmail.com>
2023-05-11 00:01:43 +0200

    Prototype commit
    Added ware component and some css

---
+++ blocket/src/components/Ware/Ware.js
@@ -0,0 +1,13 @@
+import "./Ware.css";
+
+function Ware() {
+  return (
+    <div className="Ware">
+      <button className="WareWrapper">
+        <button className="WareImage"></button>
+      </button>
+    </div>
+  );
+}
+
+export default Ware;
